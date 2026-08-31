d48683ef4281422d3134417e049af3b7d295d515
ali2develop <muhammadaliamaan515@gmail.com>
2026-08-31 16:21:40 +0500

    Delete pharmacy_master_server directory

--- pharmacy_master_server/master_data/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import MedicineSyncView, PharmacyStatusView
-
-urlpatterns = [
-    path('sync/medicines/', MedicineSyncView.as_view(), name='sync_medicines'),
-    path('pharmacy/status/', PharmacyStatusView.as_view(), name='pharmacy_status'),
-]
